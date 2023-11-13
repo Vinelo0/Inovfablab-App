f6f86c53ddb354948b20158fcf8993688475c118
Vinelo0 <113726354+Vinelo0@users.noreply.github.com>
2023-11-13 19:32:37 -0300

    Update App.tsx

--- Inov/App.tsx
+++ Inov/App.tsx
@@ -1,42 +1,48 @@
-import React, { useState } from 'react';
-import estilos from './src/estilos';
-import { Link, NavigationContainer } from '@react-navigation/native';
-import Video from 'react-native-video';
-import {View, TouchableHighlight, ScrollView, TextInput, Text, Image, ImageBackground, StyleSheet, Linking} from 'react-native';
-import { TouchableOpacity } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import estilos from "./src/estilos";
+import { Link, NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import Home from "./src/Home";
+import Login from "./src/Login";
+import {
+  View,
+  TouchableHighlight,
+  ScrollView,
+  TextInput,
+  Text,
+  Image,
+  ImageBackground,
+  StyleSheet,
+  Linking,
+  TouchableOpacity,
+} from "react-native";
 
+const Stack = createStackNavigator();
 
- function App() {
-   return (
-
-      <ImageBackground source ={require("./src/assets/imagens/Fundo.jpg")} style={estilos.fundo}>
-
-        <View style={estilos.view}>
-
-          <TouchableHighlight onPress={()=>Linking.openURL("https://inovfablab.unisanta.br")}>
-            <Image source={require('./src/assets/imagens/logofablab.png')} style={estilos.logo} ></Image>
-          </TouchableHighlight>
-
-        </View>
-
-        <View style={estilos.view}>
-
-          <TouchableOpacity style={estilos.botao1} activeOpacity={0.7}>
-            <Text style ={estilos.txt}>Entrar no FabManager</Text>
-          </TouchableOpacity>
-
-          <TouchableOpacity style={estilos.botao2} activeOpacity={0.7}>
-            <Text style ={estilos.txt}>Ficha de Autorização</Text>
-          </TouchableOpacity>
-
-        </View>
-
-        <TouchableHighlight onPress={()=>Linking.openURL("https://www.unisanta.br")} style={estilos.unisanta}>
-          <Image source={require('./src/assets/imagens/unisanta.png')} style={estilos.unisanta}/>
-        </TouchableHighlight>
-
-      </ImageBackground>
-   )
-  }
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{
+            title: "Home",
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="Login"
+          component={Login}
+          options={{
+            title: "Login",
+            headerShown: true,
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
 
 export default App;
