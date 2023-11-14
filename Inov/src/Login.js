68b9a1469f2e256e7151ac8171b57de3e081c8a9
Vinelo0 <113726354+Vinelo0@users.noreply.github.com>
2023-11-14 13:23:55 -0300

    Update Login.js

--- Inov/src/Login.js
+++ Inov/src/Login.js
@@ -1,8 +1,5 @@
-import { StatusBar } from "expo-status-bar";
-import React, { useState } from "react";
+import React from "react";
 import estilos from "./estilos";
-import { Link, NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
 import {
   View,
   TouchableHighlight,
@@ -18,20 +15,68 @@ import {
 
 function Login({ navigation }) {
   return (
-    <View
-      style={estilos.viewLogin}
-    >
+    <View style={estilos.viewLogin}>
       <ImageBackground
         source={require("./assets/imagens/Fundo.jpg")}
-        style={[estilos.fundo,{opacity:0.5}]}
-      >
-        {/* <TextInput style={estilos.loginInp}></TextInput> */}
-        <View style={estilos.viewLogin2}>
-
-          
+        style={[estilos.fundo, { opacity: 0.2, backgroundColor: "#000" }]}
+      ></ImageBackground>
+      <View style={estilos.viewLoginWhite}>
+        <Text
+          style={{
+            color: "#00f",
+            fontSize: 16,
+            fontWeight: "bold",
+            position: "absolute",
+            top: 20,
+          }}
+        >
+          LOGIN
+        </Text>
+        <View style={estilos.loginViewGrey}>
+          <Text style={estilos.loginLabelInput}>Usuário</Text>
+          <View
+            style={{
+              flexDirection: "row",
+              justifyContent: "center",
+              alignItems: "center",
+              backgroundColor: "#eee",
+            }}
+          >
+            <Image
+              source={require("./assets/imagens/email.png")}
+              style={estilos.loginImageInput}
+            ></Image>
+            <TextInput
+              style={[estilos.loginInp, { flex: 1 }]}
+              placeholder="Usuario"
+            ></TextInput>
+          </View>
+          <Text style={estilos.loginLabelInput}>Senha</Text>
+          <View
+            style={{
+              flexDirection: "row",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <Image
+              source={require("./assets/imagens/senha.png")}
+              style={estilos.loginImageInput}
+            ></Image>
+            <TextInput style={estilos.loginInp} placeholder="Senha"></TextInput>
+          </View>
+          <View style={{ alignItems: "center", marginTop: 15 }}>
+            <TouchableOpacity
+              style={estilos.loginConfirma}
+              activeOpacity={0.7}
+              onPress={() => navigation.navigate("Home")}
+            >
+              <Text style={estilos.txt}>CONFIRMAR</Text>
+            </TouchableOpacity>
+          </View>
         </View>
-      </ImageBackground>
-      
+        <Text style={estilos.viewTeste} onPress={() => navigation.navigate("Home")}>Criar Conta</Text>
+      </View>
     </View>
   );
 }
