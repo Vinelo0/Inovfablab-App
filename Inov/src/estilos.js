1e9b5054dea92f7a7f9aabb965adad54522dd52b
Vinelo0 <113726354+Vinelo0@users.noreply.github.com>
2023-11-19 13:23:54 -0300

    Update estilos.js

--- Inov/src/estilos.js
+++ Inov/src/estilos.js
@@ -1,7 +1,8 @@
 import React from "react";
-import { StyleSheet } from "react-native";
+import { StyleSheet, StatusBar } from "react-native";
 
 const estilos = StyleSheet.create({
+  // INICIO TELA HOME ------------------------------------------------------------------------------------------------
   fundo: {
     flexGrow: 1,
     resizeMode: "cover",
@@ -71,7 +72,9 @@ const estilos = StyleSheet.create({
     bottom: 3,
     right: 3,
   },
+  // FIM TELA HOME
 
+  // INICIO TELA DE LOGIN ------------------------------------------------------------------------------------------------
   loginInp: {
     width: 150,
     height: 40,
@@ -80,7 +83,6 @@ const estilos = StyleSheet.create({
     backgroundColor: "#fff",
     padding: 10,
     color: "#000",
-    
   },
   viewLogin: {
     backgroundColor: "#fff",
@@ -117,27 +119,133 @@ const estilos = StyleSheet.create({
     padding: 10,
     borderWidth: 1,
     borderRadius: 10,
-    
   },
-  loginConfirma:{
-    minWidth:140,
-    minHeight:40,
-    backgroundColor:'#9e2626',
-    color:'#fff',
-    borderRadius:10,
-    borderWidth:1,
-    padding:2,
-    justifyContent:'center'
+  loginConfirma: {
+    minWidth: 140,
+    minHeight: 40,
+    backgroundColor: "#9e2626",
+    color: "#fff",
+    borderRadius: 10,
+    borderWidth: 1,
+    padding: 2,
+    justifyContent: "center",
   },
-  viewTeste:{
-    marginTop:15,
-    color:'#00f',
+  underline: {
+    marginTop: 15,
+    color: "#00f",
     fontSize: 20,
-    textDecorationLine:'underline',
+    textDecorationLine: "underline",
     justifyContent: "center",
     textAlign: "center",
-  }
+  },
+  // FIM DA TELA DE LOGIN
+
+  //INICIO TELA AUTORIZAÇÃO ------------------------------------------------------------------------------------------------
+
+  //VIEW GERAL RED AUTORIZAÇÃO
+  viewAutorizacao: {
+    backgroundColor: "#9e2626",
+    width: "100%",
+    height: "100%",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  //CONFIGURAÇÕES DO SCROLL
+  scrollAutorizacao: {
+    paddingLeft: 10,
+    paddingRight: 10,
+    marginBottom: 10,
+  },
+  //ICONE DE VOLTAR
+  iconVoltar: {
+    width: 45,
+    height: 45,
+    justifyContent: "center",
+    flex: 1
+  },
+  //TEXT DO TOPO
+  autorizacaoTitle: {
+    color: '#fff',
+    fontSize: 23,
+    flex: 5,
+  },
+  //VIEW COM BOTÃO DE VOLTAR E TEXT
+  viewAutorizacaoTopo: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 30,
+    marginBottom: 10,
+    marginLeft: 30,
+
+  },
+  //TEXT INPUT DA FICHA
+  autorizacaoInput: {
+    borderRadius: 15,
+    borderWidth: 3,
+    backgroundColor: "white",
+    padding: 10,
+    margin: 10,
+    color: "#000",
+    minHeight: 40,
+    borderColor: '#2b156c'
+  },
+  //PARTE BRANCA
+  limiteScroll: {
+    height: '80%',
+    borderWidth: 7,
+    borderRadius: 25,
+    borderColor: "#2b156c",
+    backgroundColor: 'white',
+    width: '90%',
+    maxHeight: '95%',
+    borderRadius: 20,
+    padding: 15,
+  },
+  //FIM TELA AUTORIZAÇÃO
+  viewMaquinas: {
+    backgroundColor: 'white',
+    width: '100%',
+    height: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  image: {
+    width: '90%',
+    height: 200,
+    borderColor: 'white',
+    borderWidth: 2,
+    borderRadius: 10,
+  },
+  scrollMaquinas: {
+    paddingLeft: 10,
+    paddingRight: 10,
+    marginBottom: 10,
+  },
+  grid: {
+    width: '100%',
+    height:300,
+    alignItems:'center',
+    borderWidth:2,
+    borderRadius:25,
+    padding:5,
+    marginTop:10,
+    marginBottom:20,
+  },
+  info:{
+    flexDirection:'row',
+    height: 25,
+    padding: 0,
+    justifyContent:'center',
+    alignItems:'center',
+  },
+  containerMaquinas:{
+    width:'100%',
+    height:'90%',
+    alignItems:'center',
+  },
 });
 
 export default estilos;
+
 
